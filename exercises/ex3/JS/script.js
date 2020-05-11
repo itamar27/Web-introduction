c29eecb7b538819e0e92933dbef1f7e9e9bfc273
Itamar Yarden <itamary9@gmail.com>
2020-05-11 20:11:21 +0300

    ex3

---
+++ exercises/ex3/JS/script.js
@@ -0,0 +1,40 @@
+var sizeName = 18;
+
+
+ var createBox = function(){
+
+    var arr = "";
+    for(var i = 0; i < sizeName; i ++)
+    {
+        
+        arr += "<article></article>";
+    }
+
+    return arr;
+};
+
+
+
+var cont =  document.getElementsByClassName("container")[0];
+cont.innerHTML = createBox();
+var first = cont.firstElementChild;
+
+
+first.onmouseover = function(){
+    first.style.backgroundImage = "url(../images/I.png)";
+}
+
+first.onmouseout = function(){
+    first.style.backgroundImage = null;
+}
+
+function on(){
+    for(var i = 0; i < 6; i++)
+        cont.getElementsByTagName("article")[i].style.backgroundColor = "black";
+}
+
+function off(){
+    for(var i = 0; i < 6; i++)
+        cont.getElementsByTagName("article")[i].style.background = "none";
+}
+
