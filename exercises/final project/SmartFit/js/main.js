f3eaf2d6a63fadba719c5bdd53593964bd241d2b
Itamar Yarden <itamary9@gmail.com>
2020-05-20 15:36:15 +0300

    SmartFit

--- exercises/final project/SmartFit/js/main.js
+++ exercises/final project/SmartFit/js/main.js
@@ -1,3 +1,19 @@
-$('#workoutBtn').click(function() {
-  window.location='./form.html';
+$('#workoutBtn').click(function () {
+  window.location = './form.html';
 });
+
+
+//------------------------form elemnt-----------------------------
+
+
+
+//----------------------------workout cards trainee----------------------
+
+(function initializeCards() {
+  var cards = "";
+  for (var i = 0; i < 6; i++) {
+      cards += ' <div class="card text-center workoutList"><h2>Workout in action:</h2><h5>25 participants in yoga class</h5><p>Some text..</p><p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscingelit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco.</p></div>';
+  }
+  var main = document.getElementsByClassName("workoutcards")[0];
+  main.innerHTML = cards;
+}())
