81658e099fe9633a4c08b346672d010514c877b9
Itamar Yarden <itamary9@gmail.com>
2020-05-21 16:44:41 +0300

    SmartFit

--- exercises/final project/SmartFit/js/main.js
+++ exercises/final project/SmartFit/js/main.js
@@ -1,30 +1,81 @@
+var NumOfWorkouts = 7
+
 $('#workoutBtn').click(function () {
   window.location = './form.html';
 });
 
+//----------------------------workout card listTrainee----------------------
+var main = document.getElementsByClassName("workoutcards")[0];
+if (main != null)
+  main.innerHTML = initializeCards();
 
-//------------------------form elemnt-----------------------------
-
-
-
-//----------------------------workout card list trainee----------------------
-
-(function initializeCards() {
-  var cards = "";
-  for (var i = 0; i < 6; i++) {
-      cards += ' <div class="card text-center workoutList"><h2>Workout in action:</h2><h5>25 participants in yoga class</h5><p>Some text..</p><p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscingelit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco.</p></div>';
+function initializeCards() {
+  var cards = [];
+  var head = '<div class="card text-center workoutList allBox">';
+  var head1 = '<a href="workoutDetails.html" class="link" title="See full details"><h4 >Full body weight lifting</h4></a>';
+  var head2 = '<h6>All time crusher</h6>';
+  var head3 = '<p>Diffuclty level: 9</p>'
+  var close = '</div>';
+  for (var i = 0; i < NumOfWorkouts; i++) {
+    cards += head + head1 + head2 + head3 + close;
   }
-  var main = document.getElementsByClassName("workoutcards")[0];
-  main.innerHTML = cards;
-}())
-
+  return cards;
+}
 
 //----------------------------workout details trainee----------------------
-(function(){
-  var txt1 = '<h6 class="col-4 text-center">Name: Circuite full body</h6>';
-  var txt2 = '<h6 class="col-4 text-center">Difficulty: 6</h6>';
-  var txt3 = '<h6 class="col-4 text-center">....</6>';
-  
-  $("#headerRow").append(txt1,txt2,txt3);
+(function traineeWorkOutCards() {
+  var txt2 = '<p class="col-md-6 text-center">All time crusher fullbody for serious trainees with base knowladge in swinging KB, and Bar</p>';
+  var txt3 = '<p class="col-md-6 text-center"> 6</p>';
+
+  $(".headerRow").append(txt2, txt3);
+
+
+
+  var card1 = '<div class="cardWO allBox text-center"></div>'
+  for (var i = 0; i < 6; i++)
+    $(".cardEx").append(card1);
+
+
+  var ex = 'Ex';
+  var device = ['Bench press', 'Dumbell', 'KB swing', 'Squat-Bar', 'Deadlift', 'Pull-Ups'];
+  var sets = ['3', '2', '4', '3', '2', '4'];
+  var reps = ['10-12', '6-8', '12-14', '6-8', '6-8', '12-14'];
+  var descrepition = ['hard hard', 'hard hard', 'hard hard', 'hard hard' ,'hard hard', 'hard hard'];
+
+  var cardEx = document.getElementsByClassName("cardWO");
+  for (var i = 0; i < 6; i++) {
+    cardEx[i].innerHTML = '<h3>Ex' + (i + 1) + "</h3>";
+    cardEx[i].innerHTML += "<p>" + device[i] + "</p>";
+    cardEx[i].innerHTML += "<p>" + sets[i] + "</p>";
+    cardEx[i].innerHTML += "<p>" + reps[i] + "</p>";
+    cardEx[i].innerHTML += "<p>" + descrepition[i] + "</p>";
+    
+  }
+
 }())
 
+
+//---------------------------Workout form Trainer---------------//
+
+var numOfClicks = 0;
+var original = document.getElementById("duplicator");
+var clones = [original];
+
+$(".addNewEx").click(function () {
+
+  var clone = original.cloneNode(true);
+  clone.id = "duplicator" + ++numOfClicks;
+  var txt = '<h6>Ex' + (numOfClicks + 1) + '</h6>';
+  clone.childNodes[1].innerHTML = txt;
+  clone.firstChild.innerHTML = txt;
+  clones.push(clone);
+  original.parentNode.appendChild(clone);
+});
+
+$(".removeEx").click(function () {
+  if (numOfClicks != 0) {
+    original.parentNode.removeChild(clones[numOfClicks])
+    clones.pop();
+    numOfClicks--;
+  }
+})
